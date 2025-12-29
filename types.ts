ed641dbbd7d4baf143e9a6925355527f43a0bead
Aleix Rosinach <bepescode@gmail.com>
2025-12-29 21:05:50 +0100

    Create types.ts

---
+++ types.ts
@@ -0,0 +1,8 @@
+export interface ChatMessage {
+    role: "system" | "user" | "assistant";
+    content: string;
+}
+export interface AIService {
+    name: string;
+    chat: (messages: ChatMessage[]) => Promise<AsyncIterable<string>>;
+}
